dbb0abde84cb1acea9f02e507b340a71c8bfd287
pengyu.huang <pengyu.huang@zuolin.com>
2020-05-18 12:23:00 +0800

    2020年5月18日：增加了一个下划线或者空格转驼峰的方法

---
+++ src/pers/huangpy/main/core/coreApp.java
@@ -0,0 +1,14 @@
+package pers.huangpy.main.core;
+
+import pers.huangpy.main.appointtask.TaskUnit;
+import pers.huangpy.main.util.ClameUtil;
+
+public class coreApp {
+    public static void main(String[] args){
+        //BeanToJsonUnit.beanToJsonUnit();
+        /*TaskUnit taskUnit = new TaskUnit();
+        taskUnit.taskUnit();*/
+        ClameUtil.toCamel();
+        //System.out.println(strategies.get(0).getStrategyName());
+    }
+}
